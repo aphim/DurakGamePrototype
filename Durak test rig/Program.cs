58f7651b3710592d03e29f4acd44685e4acb1fab
jacky yuan <flamingknight11@hotmail.com>
2021-02-26 15:56:21 -0500

    Set up initial turn

--- Durak test rig/Program.cs
+++ Durak test rig/Program.cs
@@ -12,61 +12,128 @@ namespace Ch10CardClient
     {
         static void Main(string[] args)
         {
+            bool playAgain = false;
 
-            //creates a new deck object
-            Deck myDeck = new Deck();
-            //create player objects
-            Player playerAI = new Player("AI");
-            Player player1;
-
-            Console.WriteLine("Please Enter you name");
-
-            string playerName = Console.ReadLine();
-
-            player1 = new Player(playerName);
-
-            //shuffle deck
-            myDeck.Shuffle();
-
-            //display shuffled deck
-            for (int i = 0; i < myDeck.getCardsRemaining(); i++)
+            do
             {
-                //displays the current card
-                Card tempCard = myDeck.GetCard(i);
-                Console.Write(tempCard.ToString());
-                if (i != myDeck.getCardsRemaining() - 1)
+                //creates a new deck object
+                Deck myDeck = new Deck();
+                //create player objects
+                Player playerAI = new Player("AI");
+                Player player1;
+
+                Console.WriteLine("Please Enter you name");
+
+                string playerName = Console.ReadLine();
+
+                player1 = new Player(playerName);
+
+                //shuffle deck
+                myDeck.Shuffle();
+
+                //display shuffled deck
+                //for (int i = 0; i < myDeck.getCardsRemaining(); i++)
+                //{
+                //    //displays the current card
+                //    Card tempCard = myDeck.GetCard(i);
+                //    Console.Write(tempCard.ToString());
+                //    if (i != myDeck.getCardsRemaining() - 1)
+                //    {
+                //        Console.Write(", ");
+                //    }
+                //    else
+                //    {
+                //        Console.WriteLine();
+                //    }
+                //}
+
+                //get the trump card
+                TrumpCard trumpCard = myDeck.getTrumpcard();
+
+                Console.WriteLine("Trump Card");
+                Console.WriteLine(trumpCard.ToString());
+
+                //initialize player1's hand
+                Console.WriteLine("");
+                Console.WriteLine(player1.playerName + "'s Hand:");
+                player1.playerHand = new Hand(myDeck);
+                //player1.playerHand.displayHand(player1.playerHand);
+
+                //initialize player2's hand
+                Console.WriteLine("");
+                Console.WriteLine(playerAI.playerName + "'s Hand:");
+                playerAI.playerHand = new Hand(myDeck);
+                //playerAI.playerHand.displayHand(playerAI.playerHand);
+
+                //initialize the field
+                Field playingField = new Field();
+                //Console.WriteLine("\nPlay the 4th card");
+                //playingField.cardPlayed(playerAI.playerHand.playCard(3));
+                //Console.WriteLine("\nDisplay the field");
+                //playingField.displayField(playingField);
+
+
+
+                //player 1's initial turn (attacker)
+                Console.WriteLine("");
+                Console.WriteLine(player1.playerName + " goes first.");
+
+                player1.playerHand.displayHand(player1.playerHand);
+
+                int selectedCard;
+
+                int.TryParse(Console.ReadLine(), out selectedCard);
+
+                playingField.cardPlayed(player1.playerHand.playCard(selectedCard));
+
+               
+
+                //player 2's initial turn (defender)
+                Console.WriteLine("");
+                Console.WriteLine("AI player's turn.");
+
+                playerAI.playerHand.displayHand(playerAI.playerHand);
+
+                int.TryParse(Console.ReadLine(), out selectedCard);
+
+                playingField.cardPlayed(playerAI.playerHand.playCard(selectedCard));
+
+                playingField.displayField(playingField);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                //for looping the game
+                Console.WriteLine("");
+                Console.WriteLine("Play again? Y or N");
+
+                string flag = Console.ReadLine();
+                if (flag == "Y" || flag == "y")
                 {
-                    Console.Write(", ");
+                    playAgain = true;
                 }
                 else
                 {
-                    Console.WriteLine();
+                    playAgain = false;
                 }
-            }
 
-            //get the trump card
-            TrumpCard trumpCard = myDeck.getTrumpcard();
-
-            Console.WriteLine(trumpCard.ToString());
-
-            //initialize player1's hand
-            Console.WriteLine("");
-            Console.WriteLine(player1.playerName + "'s Hand:");
-            player1.playerHand = new Hand(myDeck);
-            player1.playerHand.displayHand(player1.playerHand);
-
-            //initialize player2's hand
-            Console.WriteLine("");
-            Console.WriteLine(playerAI.playerName + "'s Hand:");
-            playerAI.playerHand = new Hand(myDeck);
-            playerAI.playerHand.displayHand(playerAI.playerHand);
-
-            //initialize the field
-            Field playingField = new Field();
-            Console.WriteLine("\nPlay the 4th card");
-            playingField.cardPlayed(playerAI.playerHand.playCard(3));
-            Console.WriteLine("\nDisplay the field");
-            playingField.displayField(playingField);
+            } while (playAgain);
         }
     }
 }
